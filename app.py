19e7e971b05087cd2df1101b7dd06fc40ecb6310
anubhav-nekko <anubhav@nekko.tech>
2025-02-23 13:11:34 +0530

    Nekko Website Update

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask_cors import CORS
 import requests, json
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.core.credentials import AzureKeyCredential
+from PyPDF2 import PdfReader
 
-# Define valid users in a dictionary
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
 def load_dict_from_json(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
@@ -14,14 +17,12 @@ secrets_file = "../secrets.json"
 
 SECRETS = load_dict_from_json(secrets_file)
 
-app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
-
-# Azure Document Intelligence Credentials
-GPT_ENDPOINT = SECRETS["GPT_ENDPOINT"]
-GPT_API = SECRETS["GPT_API"]
+# ========== CONFIGURATION ==========
 ENDPOINT = SECRETS["AZURE_DOC_ENDPOINT"]
 KEY = SECRETS["AZURE_DOC_KEY"]
+AZURE_OPENAI_URL = SECRETS["GPT_ENDPOINT"]
+AZURE_OPENAI_KEY = SECRETS["GPT_API"]
+
 document_analysis_client = DocumentAnalysisClient(ENDPOINT, credential=AzureKeyCredential(KEY))
 
 # Function to extract text from PDF using Azure Document Intelligence
@@ -44,17 +45,19 @@ def index():
 
 # Your function to call the LLM API
 def call_llm_api(system_message, user_query):
-    url = GPT_ENDPOINT
+    url = AZURE_OPENAI_URL
     headers = {  
         "Content-Type": "application/json",  
-        "api-key": GPT_API
-    } 
+        "api-key": AZURE_OPENAI_KEY  
+    }  
 
     # Modify system message to include extracted document info
     system_message = f"""
     You are Nekko's AI assistant. Below is the company information and product details:
     {company_info_text}
     
+    Your Job is to Always present the Users with the Latest and Exciting Product Offerings by Nekko. Your Response should be short but attention grabbing.
+    Always try to be empathetic and try to understand the customer's pain Point.
     Use this information to answer user queries. If the query is unrelated to the company or its products, respond normally.
     If a user requests the company's sales team contact details, provide them with the email 'prithvi@nekko.tech'.
     """
@@ -89,5 +92,5 @@ def chat():
 
 if __name__ == '__main__':
     # Listen on all interfaces so external requests can reach your API.
-    app.run(debug=True, host='0.0.0.0')
-    # app.run(debug=True)
+    #app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
